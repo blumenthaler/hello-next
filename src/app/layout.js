49f58d05e83be686ff3a812f7284697ba903db36
Alex Blumenthaler <blumenthaleralex@gmail.com>
2023-07-13 10:58:07 -0400

    footer timestamp solution

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,10 @@ function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          Page rendered on{' '}
+          {new Date().toLocaleString()}
+        </footer>
       </body>
     </html>
   );
